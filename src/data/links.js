cdb294604cd781de4e7e3cd26094564ca3c4e22a
Elly K <lkotlarenko@proton.me>
2024-11-08 02:04:43 -0300

    update 2 files and create 6 files

---
+++ src/data/links.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    name: "X (Twitter)",
+    url: "https://x.com/YOURUSERNAME",
+    icon: "x-icon.png",
+  },
+  {
+    name: "Spotify",
+    url: "https://open.spotify.com/user/YOURUSERNAME",
+    icon: "spotify-icon.png",
+  },
+  {
+    name: "Bluesky",
+    url: "https://bsky.app/profile/YOURUSERNAME",
+    icon: "bsky-icon.png",
+  },
+]
+// You can add more links to the links array above, put the images inside the src/images folder
+
+export default links
